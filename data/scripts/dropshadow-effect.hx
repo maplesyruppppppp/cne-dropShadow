a515aaa598beedf0349d43d333cb2dba8e617073
maplesyruppppppp <tylersanders0714@gmail.com>
2025-04-24 23:27:17 -0500

    Initial Commit

---
+++ data/scripts/dropshadow-effect.hx
@@ -0,0 +1,386 @@
+// le credits
+/**
+ * syrup: made the stage extension
+ * moro: ported the shader frag file
+ * nex_isdumb: made the DropShadowShader class
+ */
+
+import flixel.graphics.frames.FlxFrame;
+import flixel.math.FlxAngle;
+import funkin.game.Stage.StageCharPos;
+import funkin.backend.utils.FlxInterpolateColor;
+import openfl.display.BitmapData;
+import haxe.xml.Access;
+
+if (!Options.gameplayShaders)
+{
+    disableScript();
+    return;
+}
+
+var __dsShaderAttNames:Array<String> = [
+    "ds_applyShader",
+    "brightness", "hue", "contrast", "saturation", "ds_color",
+    "ds_angle", "ds_antialiasAmt", "ds_strength", "ds_distance", "ds_threshold",
+    "ds_altMask", "ds_maskThreshold", "ds_applyAltmask"
+];
+public var dsShaderCharsAtts:Array<Array<Dynamic>> = [];
+
+function onStageNodeParsed(event)
+{
+    var sprite = event.sprite;
+    var node = event.node;
+
+    if (sprite is FlxSprite)
+    {
+        var atts = getDSShaderAttFromNode(node);
+
+        if (!atts[0]) return;
+        initDSShader(atts[1], atts[2], atts[3], atts[4], atts[5], atts[6], atts[7], atts[8], atts[9], atts[10], atts[11], atts[12], atts[13], sprite);
+    }
+    else
+    {
+        var map = event.stage.characterPoses;
+
+        for (char in map.keys()) if (map[char] == event.sprite)
+        {
+            dsShaderCharsAtts[getCharPosIndex(char)] = getDSShaderAttFromNode(node);
+            break;
+        }
+    }
+}
+
+function create() if (strumLines != null) for (i => atts in dsShaderCharsAtts) if(atts != null) for (char in strumLines.members[i]?.characters)
+{
+    if (!atts[0]) return;
+    initDSShader(atts[1], atts[2], atts[3], atts[4], atts[5], atts[6], atts[7], atts[8], atts[9], atts[10], atts[11], atts[12], atts[13], char);
+}
+
+public function getCharPosIndex(charPos:String):Int
+    return switch(charPos) { case "dad": 0; case "boyfriend": 1; default: 2; };
+
+public function getDSShaderAttFromNode(node:Access):Array<Dynamic>
+    return
+    [
+        CoolUtil.getAtt(node, "ds_applyShader") == "true",
+        getDSShaderAtt(CoolUtil.getAtt(node, "brightness")),
+        getDSShaderAtt(CoolUtil.getAtt(node, "hue")),
+        getDSShaderAtt(CoolUtil.getAtt(node, "contrast")),
+        getDSShaderAtt(CoolUtil.getAtt(node, "saturation")),
+        CoolUtil.getAtt(node, "ds_color"),
+        getDSShaderAtt(CoolUtil.getAtt(node, "ds_angle")),
+        getDSShaderAtt(CoolUtil.getAtt(node, "ds_antialiasAmt")),
+        getDSShaderAtt(CoolUtil.getAtt(node, "ds_strength")),
+        getDSShaderAtt(CoolUtil.getAtt(node, "ds_distance")),
+        getDSShaderAtt(CoolUtil.getAtt(node, "ds_threshold")),
+        CoolUtil.getAtt(node, "ds_altMask"),
+        getDSShaderAtt(CoolUtil.getAtt(node, "ds_maskThreshold")),
+        CoolUtil.getAtt(node, "ds_applyAltMask") == "true"
+    ];
+
+public function getDSShaderAtt(att:String):Float
+    return att?.length > 0 ? Std.parseFloat(att) : 0;
+
+public function initDSShader(brightness:Float, hue:Float, contrast:Float, saturation:Float, color:String, angle:Float, antialiasAmt:Float, strength:Float, 
+    distance:Float, threshold:Float, altMask:String, maskThreshold:Float, applyAltMask:Bool, sprite:FlxSprite):CustomShader
+{
+    var dropShadow = getDropShadow();
+
+    dropShadow.setAdjustColor(brightness, hue, contrast, saturation);
+    dropShadow.color = FlxColor.fromString(color);
+
+    dropShadow.angle = angle;
+    dropShadow.strength = strength;
+    dropShadow.distance = distance;
+    dropShadow.threshold = threshold;
+    dropShadow.antialiasAmt = antialiasAmt;
+
+    dropShadow.useAltMask = applyAltMask;
+    if (altMask != null) dropShadow.altMaskImage = Assets.getBitmapData(Paths.image(altMask));
+    dropShadow.maskThreshold = maskThreshold;
+
+    sprite.shader = dropShadow.shader;
+
+    dropShadow.set_attachedSprite(sprite);
+    sprite.animation.callback = function()
+    {
+        dropShadow.set_attachedSprite(sprite);
+    };
+
+    trace(Assets.getBitmapData(Paths.image(altMask)));
+}
+
+// USE THIS FUNCTION, DONT USE new DropShadowShader()!!!  - Nex
+public function getDropShadow():DropShadowShader {
+    var fucker = new DropShadowShader();
+
+    fucker.shader = new CustomShader("DropShadow");
+
+    fucker.angle = 0;
+    fucker.strength = 1;
+    fucker.distance = 15;
+    fucker.threshold = 0.1;
+
+    fucker.baseHue = 0;
+    fucker.baseSaturation = 0;
+    fucker.baseBrightness = 0;
+    fucker.baseContrast = 0;
+
+    fucker.antialiasAmt = 2;
+
+    fucker.useAltMask = false;
+
+    fucker.shader.angOffset = 0;
+
+    fucker.color = null;
+    fucker.altMaskImage = null;
+    fucker.maskThreshold = 0;
+    fucker.attachedSprite = null;
+
+    return fucker;
+}
+
+/*
+    TAKEN FROM VSLICE AND ADAPTED FOR CNE'S HSCRIPT!!!  - Nex
+*/
+class DropShadowShader
+{
+    /*
+        The DropShadow shader.
+    */
+    public var shader:CustomShader;
+
+    /*
+        The color of the drop shadow.
+    */
+    public var color:FlxColor;
+
+    /*
+        The angle of the drop shadow.
+
+        for reference, depending on the angle, the affected side will be:
+        0 = RIGHT
+        90 = UP
+        180 = LEFT
+        270 = DOWN
+    */
+    public var angle:Float;
+
+    /*
+        The distance or size of the drop shadow, in pixels,
+        relative to the texture itself... NOT the camera.
+    */
+    public var distance:Float;
+
+    /*
+        The strength of the drop shadow.
+        Effectively just an alpha multiplier.
+    */
+    public var strength:Float;
+
+    /*
+        The brightness threshold for the drop shadow.
+        Anything below this number will NOT be affected by the drop shadow shader.
+        A value of 0 effectively means theres no threshold, and vice versa.
+    */
+    public var threshold:Float;
+
+    /*
+        The amount of antialias samples per-pixel,
+        used to smooth out any hard edges the brightness thresholding creates.
+        Defaults to 2, and 0 will remove any smoothing.
+    */
+    public var antialiasAmt:Float;
+
+    /*
+        Whether the shader should try and use the alternate mask.
+        False by default.
+    */
+    public var useAltMask:Bool;
+
+    /*
+        The image for the alternate mask.
+        At the moment, it uses the blue channel to specify what is or isnt going to use the alternate threshold.
+        (its kinda sloppy rn i need to make it work a little nicer)
+        TODO: maybe have a sort of "threshold intensity texture" as well? where higher/lower values indicate threshold strength..
+    */
+    public var altMaskImage:BitmapData;
+
+    /*
+        An alternate brightness threshold for the drop shadow.
+        Anything below this number will NOT be affected by the drop shadow shader,
+        but ONLY when the pixel is within the mask.
+    */
+    public var maskThreshold:Float;
+
+    /*
+        The FlxSprite that the shader should get the frame data from.
+        Needed to keep the drop shadow shader in the correct bounds and rotation.
+    */
+    public var attachedSprite:FlxSprite;
+
+    /*
+        The hue component of the Adjust Color part of the shader.
+    */
+    public var baseHue:Float;
+
+    /*
+        The saturation component of the Adjust Color part of the shader.
+    */
+    public var baseSaturation:Float;
+
+    /*
+        The brightness component of the Adjust Color part of the shader.
+    */
+    public var baseBrightness:Float;
+
+    /*
+        The contrast component of the Adjust Color part of the shader.
+    */
+    public var baseContrast:Float;
+
+    /*
+        Sets all 4 adjust color values.
+    */
+    public function setAdjustColor(b:Float, h:Float, c:Float, s:Float)
+    {
+        set_baseBrightness(b);
+        set_baseHue(h);
+        set_baseContrast(c);
+        set_baseSaturation(s);
+    }
+
+    public function set_baseHue(val:Float):Float
+    {
+        shader.hue = baseHue = val;
+        return val;
+    }
+
+    public function set_baseSaturation(val:Float):Float
+    {
+        shader.saturation = baseSaturation = val;
+        return val;
+    }
+
+    public function set_baseBrightness(val:Float):Float
+    {
+        shader.brightness = baseBrightness = val;
+        return val;
+    }
+
+    public function set_baseContrast(val:Float):Float
+    {
+        shader.contrast = baseContrast = val;
+        return val;
+    }
+
+    public function set_threshold(val:Float):Float
+    {
+        shader.thr = threshold = val;
+        return val;
+    }
+
+    public function set_antialiasAmt(val:Float):Float
+    {
+        shader.AA_STAGES = antialiasAmt = val;
+        return val;
+    }
+
+    public function set_color(col:FlxColor):FlxColor
+    {
+        var lerpColor = new FlxInterpolateColor(color = col);  // some FlxColor stuff are abstracts, so lets use cne's FlxInterpolateColor  - Nex
+        shader.dropColor = [lerpColor.red, lerpColor.green, lerpColor.blue];
+        return color;
+    }
+
+    public function set_angle(val:Float):Float
+    {
+        shader.ang = (angle = val) * FlxAngle.TO_RAD;
+        return angle;
+    }
+
+    public function set_distance(val:Float):Float
+    {
+        shader.dist = distance = val;
+        return val;
+    }
+
+    public function set_strength(val:Float):Float
+    {
+        shader.str = strength = val;
+        return val;
+    }
+
+    public function set_attachedSprite(spr:FlxSprite):FlxSprite
+    {
+        updateFrameInfo((attachedSprite = spr)?.frame);
+        return spr;
+    }
+
+    /*
+        Loads an image for the mask.
+        While you *could* directly set the value of the mask, this function works for both HTML5 and desktop targets.
+    */
+    public function loadAltMask(path:String)
+    {
+        #if html5
+        BitmapData.loadFromFile(path).onComplete(function(bmp:BitmapData) {
+            altMaskImage = bmp;
+        });
+        #else
+        altMaskImage = BitmapData.fromFile(path);
+        trace(altMaskImage);
+        #end
+    }
+
+    /*
+        Should be called on the animation.callback of the attached sprite.
+        TODO: figure out why the reference to the attachedSprite breaks on web??
+    */
+    public function onAttachedFrame(name, frameNum, frameIndex)
+    {
+        if (attachedSprite != null) updateFrameInfo(attachedSprite.frame);
+    }
+
+    /*
+        Updates the frame bounds and angle offset of the sprite for the shader.
+    */
+    public function updateFrameInfo(frame:FlxFrame)
+    {
+        var isNull = frame == null;
+
+        // NOTE: uv.width is actually the right pos and uv.height is the bottom pos
+        shader.uFrameBounds = isNull ? null : [frame.uv.x, frame.uv.y, frame.uv.width, frame.uv.height];
+
+        // if a frame is rotated the shader will look completely wrong lol
+        shader.angOffset = isNull ? 0 : frame.angle * FlxAngle.TO_RAD;
+    }
+
+    public function set_altMaskImage(_bitmapData:BitmapData):BitmapData
+    {
+        shader.altMask = _bitmapData;
+        return _bitmapData;
+    }
+
+    public function set_maskThreshold(val:Float):Float
+    {
+        shader.thr2 = maskThreshold = val;
+        return val;
+    }
+
+    public function set_useAltMask(val:Bool):Bool
+    {
+        var isNull = val == null;
+
+        if (!isNull)
+        {
+            shader.useMask = useAltMask = val;
+            return val;
+        }
+    }
+
+    public function new()
+    {
+        // theres a bug in public hscript that wont make setters/getters be ran in new(), its fixed in dev hscript dw like many other stuff  - Nex
+    }
+}
